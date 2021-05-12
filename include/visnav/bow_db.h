7eb3d6d69b69ca7df82afc27e30d513cd60e5812
Kerem <kerem.yildirir@tum.de>
2021-05-12 13:20:06 +0200

    insert and query done

--- include/visnav/bow_db.h
+++ include/visnav/bow_db.h
@@ -22,7 +22,7 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
 DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+FOR ANY DIREfirstCT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
 DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
 SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
 CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
@@ -33,7 +33,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #pragma once
 
 #include <fstream>
-
+#include <algorithm>
 #include <tbb/concurrent_unordered_map.h>
 #include <tbb/concurrent_vector.h>
 
@@ -42,6 +42,10 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 namespace visnav {
 
+bool compareFrameDist(std::pair<FrameCamId, double> lhs,
+                      std::pair<FrameCamId, double> rhs) {
+  return lhs.second < rhs.second;
+}
 class BowDatabase {
  public:
   BowDatabase() {}
@@ -49,9 +53,11 @@ class BowDatabase {
   inline void insert(const FrameCamId& fcid, const BowVector& bow_vector) {
     // TODO SHEET 3: add a bow_vector that corresponds to frame fcid to the
     // inverted index. You can assume the image hasn't been added before.
-    UNUSED(fcid);
-    UNUSED(bow_vector);
-    
+
+    for (unsigned long i = 0; i < bow_vector.size(); i++) {
+      std::pair<FrameCamId, WordValue> temp(fcid, bow_vector[i].second);
+      inverted_index[bow_vector[i].first].emplace_back(temp);
+    }
   }
 
   inline void query(const BowVector& bow_vector, size_t num_results,
@@ -61,9 +67,36 @@ class BowDatabase {
     // to accumulate scores and std::partial_sort for getting the closest
     // results. You should use L1 difference as the distance measure. You can
     // assume that BoW descripors are L1 normalized.
-    UNUSED(bow_vector);
-    UNUSED(num_results);
-    UNUSED(results);
+    std::unordered_map<FrameCamId, double> instances;
+
+    for (unsigned long i = 0; i < bow_vector.size(); i++) {
+      int ind = bow_vector[i].first;  // wordID
+
+      for (unsigned long j = 0; j < inverted_index.at(ind).size(); j++) {
+        double dist =
+            abs(inverted_index.at(ind)[j].second - bow_vector[i].second) -
+            abs(inverted_index.at(ind)[j].second) - abs(bow_vector[i].second);
+        if (instances[inverted_index.at(ind)[j].first] == 0) {
+          instances[inverted_index.at(ind)[j].first] = 2;
+        }
+        instances[inverted_index.at(ind)[j].first] += dist;
+      }
+    }
+    int id = 0;
+    std::vector<std::pair<FrameCamId, double>> vec(instances.begin(),
+                                                   instances.end());
+    std::sort(vec.begin(), vec.end(), compareFrameDist);
+    for (const auto& elm : vec) {
+      if (id < num_results)
+        results.emplace_back(elm);
+      else {
+        break;
+      }
+      id++;
+    }
+    // UNUSED(bow_vector);
+    // UNUSED(num_results);
+    // UNUSED(results);
   }
 
   void clear() { inverted_index.clear(); }
